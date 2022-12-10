06c0c45fbcd415ed336e2554cecbb27fe039b379
VasilevID1111 <VasilevID1111@mail.ru>
2022-12-11 03:32:33 +0500

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ for ($i = 0; $i < count($usname); $i++) {
 		<div class="form-search">
 			<select name="gettype" id="gettype" class="select-search">
 				<option value="Все">Все</option>
-				<option value="Заявки">Заявки</option>
+				<option value="Заявки">Заявки123</option>
 				<option value="Объяв123123ления">Объявления</option>
 			</select>
 			
